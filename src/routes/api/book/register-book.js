a4d12a8815d0c268f18d35f68e0c2c9fac6e942b
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-01 13:43:21 -0700

    register book

--- src/routes/api/book/register-book.js
+++ src/routes/api/book/register-book.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+const bookFieldsValidation = require("../../../middlewares/book/book-input-validator");
+const bookModel = require("../../../models/book");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 const router = express.Router();
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("salam");
+router.post("/", upload.single("image"), bookFieldsValidation(), (req, res) => {
+  const { name, description, writer, status, tags } = req.body;
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      let err = [];
+      for (let error in errors.errors) {
+        err.push(errors.errors[error].msg);
+      }
+      return res.status(400).json({ errors: err });
+    }
+    new bookModel({
+      name,
+      description,
+      writer,
+      status,
+      picPath: `/upload/${req.file.originalname}`,
+      tags,
+    }).save();
+    return res.status(201).json({ message: "created" });
+  } catch (e) {
+    return res.status(500).json({ message: "internal error" });
+  }
 });
 module.exports = router;
