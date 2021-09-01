7701d719fb405277b900da7a2a782ec3cca38c97
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-01 10:09:21 -0700

    register book

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const register = require("./auth/register");
 const login = require("./auth/login");
+const registerBook = require("./book/register-book");
+//auth
 router.use("/auth/register", register);
 router.use("/auth/login", login);
+//book
+router.use("/book/register", registerBook);
 module.exports = router;
