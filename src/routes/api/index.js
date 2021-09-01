ac7c23451e6323e2b5b38de808968c021aedd2da
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-01 13:43:02 -0700

    register tag route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const register = require("./auth/register");
 const login = require("./auth/login");
 const registerBook = require("./book/register-book");
+const registerTag = require("./book/register-tag");
 //auth
 router.use("/auth/register", register);
 router.use("/auth/login", login);
 //book
 router.use("/book/register", registerBook);
+//tag
+router.use("/tag/register", registerTag);
 module.exports = router;
