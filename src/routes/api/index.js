9c15c1a2ba242abc284b82d5cc515ed88cd99e41
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-03 11:40:59 -0700

    add user info route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const register = require("./auth/register");
 const login = require("./auth/login");
+const userInfo = require("./auth/user-info");
 const registerBook = require("./book/register-book");
 const registerTag = require("./book/register-tag");
 const getBook = require("./book/get-book");
 //auth
 router.use("/auth/register", register);
 router.use("/auth/login", login);
+router.use("/auth/user/info", userInfo);
 //book
 router.use("/book/register", registerBook);
 router.use("/book/get", getBook);
